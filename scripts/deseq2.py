fed2d7c0baf21d3a3196231d0982efed9eb732fd
abhasi <ashwini.bhasi@gmail.com>
2017-02-27 11:42:05 -0500

    WAT-222(abhasi/cgates): started creating sample_metadata file

--- scripts/deseq2.py
+++ scripts/deseq2.py
@@ -2,21 +2,23 @@
 '''
 from __future__ import print_function, absolute_import, division
 
+from scripts.watermelon_config import CONFIG_KEYS, DEFAULT_PHENOTYPE_DELIM
+
+SAMPLE_METADATA_HEADER = 'phenotype'
+
 def _split_config_list(config_string):
     config_list = []
-    for i in config_string.split('^'):
+    for i in config_string.split(DEFAULT_PHENOTYPE_DELIM):
         config_list.append(i.strip())
     return config_list
 
 def _build_sample_metadata_list(config):
     lines = []
-
-    phenotype_labels = _split_config_list(config['phenotypes'])
-    header = ['phenotype']
+    phenotype_labels = _split_config_list(config[CONFIG_KEYS.phenotypes])
+    header = [SAMPLE_METADATA_HEADER]
     header.extend(phenotype_labels)
-    
     lines.append(header)
-    for sample_name, phenotype_values in sorted(config['samples'].items()):
+    for sample_name, phenotype_values in sorted(config[CONFIG_KEYS.samples].items()):
         sample_line = [sample_name]
         sample_line.extend(_split_config_list(phenotype_values))
         lines.append(sample_line)
@@ -25,8 +27,11 @@ def _build_sample_metadata_list(config):
 def _build_contrasts_list(config, output_base_path):
     return []
 
-def build_sample_metadata(config, sample_metadata_file):
-    pass
+def build_sample_metadata(config, sample_metadata_file_name):
+    with open(sample_metadata_file_name, 'w') as metadata_file:
+        lines = _build_sample_metadata_list(config)
+        for line in lines:
+            print('\t'.join(line), file=metadata_file)
 
 def build_contrasts_file(config, output_base_path, sample_metadata_file):
     pass
--- tests/deseq2_test.py
+++ tests/deseq2_test.py
@@ -1,9 +1,10 @@
 #pylint: disable=too-many-public-methods, invalid-name, no-self-use
 from __future__ import print_function, absolute_import, division
-
+import os
 import unittest
-
+from testfixtures.tempdirectory import TempDirectory
 import scripts.deseq2 as deseq2
+
 
 class Deseq2Test(unittest.TestCase):
     def test_build_sample_metadata_list_basecase(self):
@@ -24,3 +25,19 @@ class Deseq2Test(unittest.TestCase):
         self.assertEqual(['sampleC','','male'], next(line))
         self.assertEqual(['sampleD','HF',''], next(line))
         self.assertEqual(['sampleE','',''], next(line))
+
+    def test_build_sample_metadata_basecase(self):
+        config = {'phenotypes': ' diet ^ gender ',
+                  'samples' : {'sampleA' : 'HF ^ male'}}
+
+        with TempDirectory() as temp_dir:
+            temp_dir_path = temp_dir.path
+            sample_metadata_file_name = os.path.join(temp_dir_path, 'sample_metadata.txt')
+            deseq2.build_sample_metadata(config, sample_metadata_file_name)
+            with open(sample_metadata_file_name, 'r') as actual_file:
+                lines = actual_file.readlines()
+
+        self.assertEqual(2, len(lines))
+        line = iter(lines)
+        self.assertEqual('phenotype\tdiet\tgender\n', next(line))
+        self.assertEqual('sampleA\tHF\tmale\n', next(line))

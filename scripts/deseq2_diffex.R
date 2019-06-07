208b4634326f45268333fda61b56a76c4659b625
Travis Saari <trsaari@umich.edu>
2019-06-07 17:09:33 -0400

    pass arguments from config directly to DESeq call

--- scripts/deseq2_diffex.R
+++ scripts/deseq2_diffex.R
@@ -15,6 +15,7 @@ save(snakemake, file = snakemake@params[['snakemake_rdata']])
 # opt = parse_args(OptionParser(option_list=option_list))
 
 rsem_dir = snakemake@params[['rsem_dir']]
+#rsem_dir = paste0("/nfs/med-bfx-activeprojects/trsaari/example_output_Watermelon/", rsem_dir) #TWS DEBUG
 #config_file = opt$config_file
 threads = snakemake@threads
 
@@ -46,6 +47,15 @@ named.filepaths.from.dir <- function(rsem.dir, file.extension) {
   return(fpathlist)
 }
 
+#Function to generate DESeq expression from diffex config parameters
+# DESeq.call.from.params = function(obj, paramslist) {
+#   param.kv = list()
+#   for(i in seq_along(paramslist)) { append(param.kv, paste(names(paramslist)[i], paramslist[[i]], sep="=")) }
+#   return(paste("DESeq", "obj"
+# }
+#test.call <- parse(text="DESeq(object=dds, full=~0 + pheno, reduced=~0, betaPrior=False, test='LRT')")
+#foo <- eval(test.call)
+
 # #######################################
 # # yaml parsing
 #
@@ -57,6 +67,10 @@ named.filepaths.from.dir <- function(rsem.dir, file.extension) {
 # results_dir = sprintf('%s/deseq2/01-deseq2_diffex', diffex_dir)
 # counts_dir = sprintf('%s/counts', results_dir)
 # gene_lists_dir = sprintf('%s/gene_lists', results_dir)
+
+# Get comparison info
+model_name = snakemake@wildcards[['model_name']]
+deseq2.params = snakemake@config[['diffex']][[model_name]][['DESeq2']][['DESeq2']]
 
 # Get phenotype matrix
 sample.info.file = snakemake@config[['sample_description_file']]
@@ -74,9 +88,6 @@ fc_cutoff = log2(as.numeric(snakemake@config[['diffex']][['fold_change']]))
 
 # Import count data
 gene.files.list <- named.filepaths.from.dir(rsem_dir, ".genes.results")
-cat("gene.files.list length:")
-cat(length(gene.files.list))
-quit()
 txi.rsem.gene.results <- tximport(gene.files.list, type = "rsem", txIn = F, txOut = F)
 #Some genes have length zero (what does this even mean?), causing issues with creating DESeqDataSet
 #Mike Love recommends changing these from 0 to 1; https://support.bioconductor.org/p/84304/#84368
@@ -86,55 +97,78 @@ txi.rsem.gene.results$length[txi.rsem.gene.results$length == 0] <- 1
 # Create DESeqDataSet from matrix and filter lowly expressed genes
 
 message('Initializing DESeq2 result')
-dds = DESeqDataSetFromTximport(txi = txi.rsem.gene.results, colData = pdata, design = ~0 + pheno) #TWS DEBUG - combinatoric_group doesn't exist - using this as an example, but creates issues in the for loop below
+#Import data from rsem
+dds = DESeqDataSetFromTximport(txi = txi.rsem.gene.results, colData = pdata, design = as.formula(deseq2.params$full))
 # QUESTION: Should this be more stringent?
+# TWS
 dds = dds[ rowSums(counts(dds)) > 1, ]
 
 # QUESTION: Why do this here before DESeq()?
 rld = rlog(dds, blind = FALSE)
 
-message('Calculating dispersion')
-#Normalize and calculate dispersions
-dds = DESeq(dds, betaPrior = TRUE, parallel = TRUE)
+#Parse the params for DESeq call, if it can't be converted return it as string
+deseq2.params.parsed = lapply(deseq2.params, function(x) {
+  tryCatch(eval(parse(text=x)),
+  error=function(e){
+    message(paste0(x, " cannot be parsed. Leaving as string"))
+    x=as.character(x)
+  })
+})
+#Add dds object to params list
+deseq2.params.parsed[['object']] = dds
 
-###################
-# Extract counts
-raw_counts = counts(dds) #raw
-norm_counts = counts(dds, normalized = TRUE) #raw/lib size factors
+# TWS - betaPrior cannot be used with a model that has an intercept. Don't think this should be generalized...
+# message('Calculating dispersion')
+# #Normalize and calculate dispersions
+# dds = DESeq(dds, betaPrior = F, parallel = TRUE)
 
-###################
-# Write counts
-message('Writing count files')
-raw_counts_df = as.data.frame(raw_counts)
-data.table::setDT(raw_counts_df, keep.rownames = TRUE)[] #set rownames to valid column
-setnames(raw_counts_df, 'rn', 'id')
-write.table(x = raw_counts_df, file=paste(countsDir,'raw_counts.txt', sep = '/'),
-    append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
 
-norm_counts_df = as.data.frame(norm_counts)
-data.table::setDT(norm_counts_df, keep.rownames = TRUE)[] #set rownames to valid column
-setnames(norm_counts_df, 'rn', 'id')
-write.table(x = norm_counts_df, file=paste(countsDir,'depth_normalized_counts.txt', sep = '/'),
-    append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+#Call to DESeq
+dds = do.call(DESeq, deseq2.params.parsed)
 
-rld_df = as.data.frame(assay(rld))
-data.table::setDT(rld_df, keep.rownames = TRUE)[] #set rownames to valid column
-setnames(rld_df, 'rn', 'id')
-write.table(x = rld_df, file=paste(countsDir,'rlog_normalized_counts.txt', sep = '/'),
-    append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+quit()
+
+# TWS - This should be moved out to a separate rule/script which will be executed once for all included samples
+# ###################
+# # Extract counts
+# raw_counts = counts(dds) #raw
+# # TWS - need to estimate size factors before this can be done
+# dds.withSF = estimateSizeFactors(dds)
+# norm_counts = counts(dds.withSF, normalized = TRUE) #raw/lib size factors
+#
+# ###################
+# # Write counts
+# message('Writing count files')
+# raw_counts_df = as.data.frame(raw_counts)
+# data.table::setDT(raw_counts_df, keep.rownames = TRUE)[] #set rownames to valid column
+# setnames(raw_counts_df, 'rn', 'id')
+# write.table(x = raw_counts_df, file=paste(countsDir,'raw_counts.txt', sep = '/'),
+#     append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+#
+# norm_counts_df = as.data.frame(norm_counts)
+# data.table::setDT(norm_counts_df, keep.rownames = TRUE)[] #set rownames to valid column
+# setnames(norm_counts_df, 'rn', 'id')
+# write.table(x = norm_counts_df, file=paste(countsDir,'depth_normalized_counts.txt', sep = '/'),
+#     append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
+#
+# rld_df = as.data.frame(assay(rld))
+# data.table::setDT(rld_df, keep.rownames = TRUE)[] #set rownames to valid column
+# setnames(rld_df, 'rn', 'id')
+# write.table(x = rld_df, file=paste(countsDir,'rlog_normalized_counts.txt', sep = '/'),
+#     append = FALSE, sep = '\t', col.names = TRUE, row.names = FALSE, quote = FALSE)
 
 #######################################
 # Diffex analysis in loop
 
-contrastData = read.table(file = "example_output_Watermelon/contrasts.txt", header=TRUE, sep = '\t', stringsAsFactors = FALSE, strip.white = TRUE) #TWS DEBUG
-colData <- read.table(file = "example_output_Watermelon/sample_metadata.txt" , header = TRUE, sep = '\t', stringsAsFactors = FALSE, na.strings = '', check.names = FALSE) #TWS DEBUG
+contrastData = read.table(file = "/nfs/med-bfx-activeprojects/trsaari/example_output_Watermelon/contrasts.txt", header=TRUE, sep = '\t', stringsAsFactors = FALSE, strip.white = TRUE) #TWS DEBUG
+colData <- read.table(file = "/nfs/med-bfx-activeprojects/trsaari/example_output_Watermelon/sample_metadata.txt" , header = TRUE, sep = '\t', stringsAsFactors = FALSE, na.strings = '', check.names = FALSE) #TWS DEBUG
 
 # Container for the results
 de_results = split(contrastData, contrastData$factor)
 de_results = lapply(de_results, function(de){split(de, de$base_file_name)})
 
 for (i in 1:nrow(contrastData)){
-
+i = 7 #TWS DEBUG
     #dir assignment
     dir_diffex = paste(diffexDir, contrastData$factor[i], sep = '/')
 

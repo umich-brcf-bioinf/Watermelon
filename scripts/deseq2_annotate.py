fb4f2252d8929089a01d8ed53fecab56f5ee004b
Travis Saari <trsaari@umich.edu>
2019-09-19 17:17:30 -0400

    WAT-404/WAT-405 - Overhauled annotation script - simplified/generalized

--- scripts/deseq2_annotate.py
+++ scripts/deseq2_annotate.py
@@ -1,28 +1,12 @@
 #!/usr/bin/python
 
-# adds annotations to output of: DESeq2
-
 from __future__ import print_function, absolute_import, division
 import sys
-import csv
+import os
 import datetime
 import time
 import argparse
-from collections import defaultdict
-
-WARNING_PERCENTAGE_ANNOTATED_CUTOFF = 50
-TAXONOMY = {'hg19': '9606', 'GRCh37': '9606', 'hg38': '9606', 'GRCh38': '9606',   # human
-            'mm10': '10090', 'mm9': '10090',    # mouse
-            'rn5': '10116', 'rn6': '10116',     # rat
-            'ce10': '6239', 'ce11': '6239', 'WS220': '6239', 'WBS235': '6239',   # c. elegans
-            'GRCz10' : '7955', 'Zv9': '7955',   # zebra fish
-            'TAIR9': '3702', 'TAIR10': '3702',  # arabidopis
-            'MSU6': '4530',                     # rice
-            'ecoMG1655': '511145', 'ecoUTI89': '364106', # ecoli
-            'dm6': '7227', #drosophila
-            }
-
-
+import pandas as pd
 
 def time_stamp():
     ts = time.time()
@@ -30,92 +14,57 @@ def time_stamp():
     return(st)
 
 def log(message):
-    print('{}|annotate_entrez_gene_info |{}'.format(time_stamp(), message), file=sys.stderr)
+    print('{}| annotate_gene_info |{}'.format(time_stamp(), message), file=sys.stderr)
 
 def parse_command_line_args():
     parser = argparse.ArgumentParser(
-        description='Adds NCBI gene_info annotations to diffex output of scripts/Cuffdiff_out_format_v6.pl')
+        description='Adds EntrezGeneIDs and Descrtiptions to diffex output')
 
     parser.add_argument(
-        '-i', '--geneinfo', type=str, help='path to input NCBI gene_info file', required=True)
+        '-m', '--mapping_file', type=str, help='Path to mapping file with required columns "gene_id", "entrezgene_id", "external_gene_name", "description"', required=True)
     parser.add_argument(
-        '-e', '--diffexp', type=str, help='path to input differential_expression_file', required=True)
+        '-i', '--input_file', type=str, help='Path to input file', required=True)
     parser.add_argument(
-        '-g', '--genome', type=str, help='organism taxonomy ID', required=True)
+        '-o', '--output_filename', type=str, help='Path to annotated output', required=True)
     parser.add_argument(
-        '-o', '--output_filename', type=str, help='path and file name of annotated output', required=True)
+        '--mapping_idx', type=str, help='String matching the column to use as the index of the mapping file', required=True)
+    parser.add_argument(
+        '--input_idx', type=str, help='String matching the column to use as the index of the input file', required=True)
     args = parser.parse_args()
     return args
 
-
-def check_geneinfo_matches(all_lines,
-                           matching_lines,
-                           warning_percent_annotated_cutoff=WARNING_PERCENTAGE_ANNOTATED_CUTOFF):
-    percentage_matches = int((matching_lines/all_lines)*100)
-    msg_format = 'Annotations added for {} ({}%) of rows in diffexp file'
-    msg = msg_format.format(matching_lines, percentage_matches)
-    log(msg)
-    if percentage_matches < warning_percent_annotated_cutoff:
-        msg_format = 'WARNING: Low number of annotations! (< {}% of genes were annotated)'
-        msg = msg_format.format(warning_percent_annotated_cutoff)
-        log(msg)
-
-def get_taxid(genome):
-    if genome in TAXONOMY:
-        return TAXONOMY.get(genome)
-    else:
-        msg_fmt = 'ERROR: Taxonomy id not found for -g/--genome:[{}]. Available taxonomy-genome mappings are: {}'
-        msg = msg_fmt.format(genome, TAXONOMY)
-        print(msg, file=sys.stderr)
-        sys.exit()
-
 def main():
     args = parse_command_line_args()
-    gene_info = args.geneinfo
-    gene_expr = args.diffexp
-    output_filename = args.output_filename
 
-    tax_id = get_taxid(args.genome)
+    log('Reading ID mapping info')
 
-    log('reading entrez gene info')
+    # Read in mapping info
+    attr_table = pd.read_csv(args.mapping_file, sep="\t", dtype=object)
+    try:
+        #TWS FIXME? - There are instances of biomaRt queries returning multiple results
+        #These are cases where a query ID matches equally well to multiple items from another database, so biomaRt returns them all
+        #Right now, just keeping the first occurence - default of drop_duplicates
+        attr_table = attr_table.drop_duplicates(args.mapping_idx).set_index(args.mapping_idx)
+    except KeyError:
+        print("{} is not a column in {}".format(args.mapping_idx, args.mapping_file))
+        sys.exit(os.EX_CONFIG)
 
-    #### def get_geneinfo_lines():
+    # Read in input file
+    try:
+        to_annotate = pd.read_csv(args.input_file, sep="\t", dtype=object, index_col=args.input_idx)
+    except ValueError:
+        print("{} is not a column in {}".format(args.input_idx, args.input_file))
+        sys.exit(os.EX_CONFIG)
 
-    gene_details = defaultdict(list)
-    with open(gene_info,'r') as geneinfo_file:
-        next(geneinfo_file) # skip header
-        reader=csv.reader(geneinfo_file,delimiter='\t')
+    #Add annotation columns
+    output = attr_table.join(to_annotate).reset_index()
 
-        for row in reader:
-            (geneinfo_tax_id, geneinfo_gene_id, geneinfo_gene_symbol, geneinfo_gene_description) = (row[0], row[1], row[2], row[8])
-            if geneinfo_tax_id == tax_id:
-                gene_details[geneinfo_gene_symbol] = [geneinfo_gene_id, geneinfo_gene_description]
+    #Remove redundant external_gene_name column if it's identical to gene_id
+    if output[args.mapping_idx].equals(output['external_gene_name']):
+        output.drop(columns='external_gene_name', inplace=True)
 
-    all_lines = 0
-    matching_gene_symbol_count = 0
-    with open(gene_expr, 'r') as file_to_annotate, open(output_filename, 'w') as annotated_file:
-        reader=csv.reader(file_to_annotate,delimiter='\t')
-        for row in reader:
-            all_lines += 1
-            row = [col.strip() for col in row]
-            if row[0] == 'id':
-                row[0] = 'gene_symbol'
-                row.insert(1, 'gene_id')
-                row.insert(2, 'gene_desc')
-                print('\t'.join(row), file=annotated_file)
-            else:
-                left = row[:1]
-                right = row[1:]
-                gene_name = row[0]
-                if gene_name in gene_details:
-                    gene_id_symbol = gene_details[gene_name]
-                    matching_gene_symbol_count += 1
-                else:
-                    gene_id_symbol = ['.','.']
-                outline = "\t".join(left + gene_id_symbol + right)
-                print(outline, file=annotated_file)
+    output.to_csv(args.output_filename, sep="\t", na_rep="NA", index=False)
 
-    check_geneinfo_matches(all_lines, matching_gene_symbol_count)
     log('done')
 
 if __name__ == '__main__':

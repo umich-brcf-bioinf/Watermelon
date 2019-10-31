34b4e2d0b7883161281d913841726412bd78b9d5
Travis Saari <trsaari@umich.edu>
2019-10-31 11:07:55 -0400

    WAT-394 Removed unlocker script and its tests

--- unlocker-runner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-"""Convenience wrapper for running unlocker directly from source tree."""
-
-from scripts.unlocker import main
-
-if __name__ == '__main__':
-    exit(main())

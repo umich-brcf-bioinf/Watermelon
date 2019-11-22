a05ffaa63b2f9db0ce7aa0f7ea691943a0c2e5ae
Travis Saari <trsaari@umich.edu>
2019-11-22 14:55:23 -0500

    dryrun test added samplesheet

--- tests/rule_tests/snakemake_dry_run_test/snakemake_dry_run_test.py
+++ tests/rule_tests/snakemake_dry_run_test/snakemake_dry_run_test.py
@@ -26,13 +26,17 @@ class SnakemakeDryRunTest(unittest.TestCase):
 
             example_data_dir = os.path.join(WATERMELON_BASE_DIR, 'data')
             tmp_data_dir = os.path.join(temp_dir.path, 'data')
+            #Copy example data here
             shutil.copytree(example_data_dir, tmp_data_dir)
+            #Copy samplesheet here
+            shutil.copyfile(os.path.join(WATERMELON_BASE_DIR, 'config', 'example_samplesheet.csv'), 'samplesheet.csv')
 
             config_replacement_vals = {
+                'sample_description_file': os.path.join(temp_dir.path, 'samplesheet.csv'),
                 'dirs': {'input' : os.path.join(tmp_data_dir, 'sim_reads_human_chr22')},
                 'references': {
                     'fasta' : os.path.join(tmp_data_dir, 'Homo_sapiens.GRCh38.98.chr22.gtf.gz'), # These just need to exist for dry-run
-                    'gtf' : os.path.join(tmp_data_dir, 'Homo_sapiens.GRCh38.dna_sm.chr22.fa.gz') # For real-deal, unzip them first
+                    'gtf' : os.path.join(tmp_data_dir, 'Homo_sapiens.GRCh38.dna_sm.chr22.fa.gz') # For real-deal, unzip them first & point to non-gz
                 },
                 'email' : {
                     'to': 'nobody'
@@ -40,7 +44,8 @@ class SnakemakeDryRunTest(unittest.TestCase):
             }
 
             #Create modified config in this temp dir, using example config and replacing values as needed
-            configfile_path = os.path.join(temp_dir.path, 'testcase_config.yaml')
+            #configfile_path = os.path.join(temp_dir.path, 'testcase_config.yaml')
+            configfile_path = os.path.join('/tmp', 'testcase_config.yaml')
             testing_utils.create_modified_config(EXAMPLE_CONFIGFILE_PATH, configfile_path, config_replacement_vals)
 
             command_fmt = 'snakemake --snakefile {} --configfile {} -n --config skip_validation=True {}'

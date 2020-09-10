b46f54d1be5968305f482725960745cf1d0d7b38
Travis Saari <trsaari@umich.edu>
2020-09-10 17:58:58 -0400

    WIP customized acknowledgement section

--- scripts/report_rmd.R
+++ scripts/report_rmd.R
@@ -13,6 +13,7 @@ library(knitr)
 
 project_name = snakemake@config[['report_info']][['project_name']]
 analyst_name = snakemake@config[['report_info']][['analyst_name']]
+acknowledgement_text = snakemake@config[['report_info']][['acknowledgement_text']]
 analyst_email = snakemake@config[['email']][['to']]
 
 report_dir = snakemake@params[['report_dir']]

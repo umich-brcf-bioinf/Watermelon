e85f23329d69e6e718d04644d6512bc63f7619e5
Travis Saari <tsaari987@gmail.com>
2021-09-13 15:16:21 -0400

    Refactor snakemake_functional_run_test with pytest

--- tests/snakemake_functional_run_test.py
+++ tests/snakemake_functional_run_test.py
@@ -1,95 +1,85 @@
 import os
 import pandas as pd
+import pytest
 import random
 import re
 import string
 import subprocess
 import unittest
 
+from mock import patch
 from tempfile import TemporaryDirectory
 from tests import testing_utils #local module
 
 TEST_DIR = os.path.realpath(os.path.dirname(__file__))
 WATERMELON_BASE_DIR = os.path.abspath(os.path.join(TEST_DIR, '..'))
-TMP_BASE_DIR = "/nfs/mm-isilon/bioinfcore/ActiveProjects/trsaari/tmp"
-DEBUG = 'WATERMELON_DEBUG' in os.environ
-REDIRECT_OUTPUT = ' ' if DEBUG else ' 2>/dev/null '
 
 
-class SnakemakeExampleDataTest(unittest.TestCase):
-    def setUp(self):
-        self.original_wd = os.getcwd()
-        self.addTypeEqualityFunc(pd.DataFrame, self.assertDataframeEqualEnough)
+def assertDataframeEqualEnough(a, b):
+    #https://stackoverflow.com/a/54608841
+    # allows us to use pandas testing.assert_frame_equal with mock.patch
+    # Note using check_exact=False
+    try:
+        pd.testing.assert_frame_equal(a, b, check_exact=False)
+    except AssertionError as e:
+        pytest.fail("pd.testing.assert_frame_equal with check_exact=False did not pass")
+    return True
 
-    def tearDown(self):
-        os.chdir(self.original_wd)
 
-    def assertDataframeEqualEnough(self, a, b, msg):
-        #https://stackoverflow.com/a/54344148
-        # allows us to use pandas testing.assert_frame_equal within unittest.testcase
-        # Note using check_exact=False
-        try:
-            pd.testing.assert_frame_equal(a, b, check_exact=False)
-        except AssertionError as e:
-            raise self.failureException(msg) from e
+def randompath(basepath, str_length=8):
+    letters = string.ascii_lowercase
+    randstr = ''.join(random.choice(letters) for i in range(str_length))
+    tmpstr = "tmp" + randstr
+    return os.path.join(basepath, tmpstr)
 
-    @staticmethod
-    def randompath(basepath, str_length=8):
-        letters = string.ascii_lowercase
-        randstr = ''.join(random.choice(letters) for i in range(str_length))
-        tmpstr = "tmp" + randstr
-        return os.path.join(basepath, tmpstr)
+@patch("pandas.DataFrame.__eq__", assertDataframeEqualEnough)
+def test_wminit_snakemake_run_align_qc(tmp_path):
+    os.chdir(tmp_path)
+    # with TemporaryDirectory(prefix=tmp_path) as temp_dir:
+    #     os.chdir(temp_dir)
 
-    def test_wminit_snakemake_run_align_qc(self):
-        tmp_path = self.randompath(TMP_BASE_DIR)
-        with TemporaryDirectory(prefix=tmp_path) as temp_dir:
-            os.chdir(temp_dir)
+    wminit_command_fmt = "{} --genome_build TestData --project_id test_align_qc --type align_qc --input_run_dirs {}"
+    wminit_command = wminit_command_fmt.format(
+        os.path.join(WATERMELON_BASE_DIR, "watermelon_init.py"),
+        os.path.join(WATERMELON_BASE_DIR, "data", "sim_reads_human_chr22")
+    )
+    try:
+        # 0 if good, otherwise CalledProcessError will be raised
+        return_code = subprocess.check_call(wminit_command, shell=True)
+    except subprocess.CalledProcessError as e:
+        return_code = e.returncode
 
-            wminit_command_fmt = "{} --genome_build TestData --project_id test_align_qc --type align_qc --input_run_dirs {}"
-            wminit_command = wminit_command_fmt.format(
-                os.path.join(WATERMELON_BASE_DIR, "watermelon_init.py"),
-                os.path.join(WATERMELON_BASE_DIR, "data", "sim_reads_human_chr22")
-            )
-            try:
-                # 0 if good, otherwise CalledProcessError will be raised
-                return_code = subprocess.check_call(wminit_command, shell=True)
-            except subprocess.CalledProcessError as e:
-                return_code = e.returncode
-            self.assertEqual(0, return_code)
+    assert(return_code == 0)
 
-            rm_keys = ["email", "trimming"] # Unnecessary to trim the simulated data
-            #Create modified config in this temp dir, using example config and just removing the email key
-            testing_utils.create_modified_config("config_test_align_qc.yaml", "config_test_align_qc.ready", {}, rm_keys)
-            command_fmt = "snakemake --snakefile {} --configfile {} --profile {}"
-            # --singularity-args \'-B {}\'
-            command = command_fmt.format(
-                os.path.join("Watermelon", 'align_qc.smk'),
-                "config_test_align_qc.ready",
-                os.path.join("Watermelon", "config", "profile-greatlakes"),
-                REDIRECT_OUTPUT
-            )
+    rm_keys = ["email", "trimming"] # Unnecessary to trim the simulated data
+    #Create modified config in this temp dir, using example config and just removing the email key
+    testing_utils.create_modified_config("config_test_align_qc.yaml", "config_test_align_qc.ready", {}, rm_keys)
+    command_fmt = "snakemake --snakefile {} --configfile {} --profile {}"
+    # --singularity-args \'-B {}\'
+    command = command_fmt.format(
+        os.path.join("Watermelon", 'align_qc.smk'),
+        "config_test_align_qc.ready",
+        os.path.join("Watermelon", "config", "profile-greatlakes")
+    )
 
-            return_code = subprocess.call(command, shell=True)
+    return_code = subprocess.call(command, shell=True)
 
-            self.assertEqual(return_code, 0)
+    assert(return_code == 0)
 
-            #Things to check:
-            # un-annotated rsem counts tables - FPKM, TPM (alignment_results)
-            gene_FPKM_actual_path = os.path.join(temp_dir, 'analysis_test_align_qc', 'alignment_results', '05-combine_counts', 'gene_FPKM.txt')
-            gene_FPKM_actual = pd.read_csv(gene_FPKM_actual_path, sep="\t")
-            gene_FPKM_expected_path = os.path.join(temp_dir, 'Watermelon', 'data', 'expected_results_files', 'gene_FPKM.txt')
-            gene_FPKM_expected = pd.read_csv(gene_FPKM_expected_path, sep="\t")
+    #Things to check:
+    # un-annotated rsem counts tables - FPKM, TPM (alignment_results)
+    gene_FPKM_actual_path = os.path.join(tmp_path, 'analysis_test_align_qc', 'alignment_results', '05-combine_counts', 'gene_FPKM.txt')
+    gene_FPKM_actual = pd.read_csv(gene_FPKM_actual_path, sep="\t")
+    gene_FPKM_expected_path = os.path.join(tmp_path, 'Watermelon', 'data', 'expected_results_files', 'gene_FPKM.txt')
+    gene_FPKM_expected = pd.read_csv(gene_FPKM_expected_path, sep="\t")
 
-            gene_TPM_actual_path = os.path.join(temp_dir, 'analysis_test_align_qc', 'alignment_results', '05-combine_counts', 'gene_TPM.txt')
-            gene_TPM_actual = pd.read_csv(gene_TPM_actual_path, sep="\t")
-            gene_TPM_expected_path = os.path.join(temp_dir, 'Watermelon', 'data', 'expected_results_files', 'gene_TPM.txt')
-            gene_TPM_expected = pd.read_csv(gene_TPM_expected_path, sep="\t")
+    gene_TPM_actual_path = os.path.join(tmp_path, 'analysis_test_align_qc', 'alignment_results', '05-combine_counts', 'gene_TPM.txt')
+    gene_TPM_actual = pd.read_csv(gene_TPM_actual_path, sep="\t")
+    gene_TPM_expected_path = os.path.join(tmp_path, 'Watermelon', 'data', 'expected_results_files', 'gene_TPM.txt')
+    gene_TPM_expected = pd.read_csv(gene_TPM_expected_path, sep="\t")
 
-            # import pdb; pdb.set_trace() # TWS DEBUG
-            # from nose.tools import set_trace; set_trace() # TWS DEBUG
-
-            self.assertEqual(gene_FPKM_expected, gene_FPKM_actual)
-            self.assertEqual(gene_TPM_expected, gene_TPM_actual)
+    assert(gene_FPKM_expected == gene_FPKM_actual)
+    assert(gene_TPM_expected == gene_TPM_actual)
 
             # # deseq2 counts tables (deliverables)
             # deseq2_raw_actual_path = os.path.join(temp_dir.path, 'analysis_test', 'deliverables', 'counts', 'deseq2_raw_counts.txt')
